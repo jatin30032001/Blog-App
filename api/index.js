ac248463f574d02bba303ff73120173d451b6c90
jatin30032001 <jrsaini2090@gmail.com>
2022-07-21 17:15:04 +0530

    posts CRUD

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@
  const mongoose = require("mongoose");
  const authRoute = require("./routes/auth")
  const userRoute = require("./routes/users")
+ const postRoute = require("./routes/posts")
 
 
  dotenv.config();
@@ -25,6 +26,7 @@ mongoose.connect(process.env.MONGO_URL , {
 
 app.use("/api/auth" , authRoute);
 app.use("/api/users" , userRoute);
+app.use("/api/posts" , postRoute);
 
 
  app.listen("5000 ", ()=>{
