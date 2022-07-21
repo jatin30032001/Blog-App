3bc6ebb55c04df63bcb14e90f8258416b143b73b
jatin30032001 <jrsaini2090@gmail.com>
2022-07-21 13:27:58 +0530

    Models Created

--- api/index.js
+++ api/index.js
@@ -1,9 +1,23 @@
  const express = require("express");
 
  const app = express();
+ const dotenv  = require("dotenv");
+ const mongoose = require("mongoose");
 
 
+ dotenv.config();
  console.log("Hello In");
+ 
+
+mongoose.connect(process.env.MONGO_URL , {
+    useNewUrlParser :true,
+    useUnifiedTopology:true,
+    // useCreateIndex :true , 
+}) 
+.then(console.log("Connected To Mongo DB"))
+.catch((err)=> console.log(err)) 
+// mongoose.set('useFindAndModify', false)
+
 
  app.listen("5000 ", ()=>{
   
