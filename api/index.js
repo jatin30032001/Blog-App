8b88c6acf54c7ec7820ba567531ce1c718b005e0
jatin30032001 <jrsaini2090@gmail.com>
2022-07-21 19:35:01 +0530

    Categories and Uploading image

--- api/index.js
+++ api/index.js
@@ -6,6 +6,8 @@
  const authRoute = require("./routes/auth")
  const userRoute = require("./routes/users")
  const postRoute = require("./routes/posts")
+ const categoryRoute = require("./routes/categories")
+ const multer = require("multer")
 
 
  dotenv.config();
@@ -24,11 +26,28 @@ mongoose.connect(process.env.MONGO_URL , {
 // mongoose.set('useFindAndModify', false)
 
 
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+      cb(null, "images");
+    },
+    filename: (req, file, cb) => {
+      cb(null, req.body.name);
+    },
+  });
+  
+  const upload = multer({ storage: storage });
+  app.post("/api/upload", upload.single("file"), (req, res) => {
+    res.status(200).json("File has been uploaded");
+  });
+  
+
+
 app.use("/api/auth" , authRoute);
 app.use("/api/users" , userRoute);
 app.use("/api/posts" , postRoute);
+app.use("/api/categories" , categoryRoute);
 
-
+console.log("Jatin");
  app.listen("5000 ", ()=>{
   
     console.log("Backend is Running")
