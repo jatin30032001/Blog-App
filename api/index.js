f949c249158786e9339b526feb1be191ff2539a7
jatin30032001 <jrsaini2090@gmail.com>
2022-07-21 16:35:10 +0530

    Authentiction Added

--- api/index.js
+++ api/index.js
@@ -3,10 +3,14 @@
  const app = express();
  const dotenv  = require("dotenv");
  const mongoose = require("mongoose");
+ const authRoute = require("./routes/auth")
+ const userRoute = require("./routes/users")
 
 
  dotenv.config();
- console.log("Hello In");
+//  console.log("Hello In");
+
+app.use(express.json());
  
 
 mongoose.connect(process.env.MONGO_URL , {
@@ -17,6 +21,10 @@ mongoose.connect(process.env.MONGO_URL , {
 .then(console.log("Connected To Mongo DB"))
 .catch((err)=> console.log(err)) 
 // mongoose.set('useFindAndModify', false)
+
+
+app.use("/api/auth" , authRoute);
+app.use("/api/users" , userRoute);
 
 
  app.listen("5000 ", ()=>{
