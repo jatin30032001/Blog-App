9e211e30bd489b0ba8ad4047d4e829fffb39c51f
jatin30032001 <jrsaini2090@gmail.com>
2022-07-24 22:16:00 +0530

    Commit

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -5,7 +5,7 @@ export default function Posts({ posts }) {
   return (
     <div className="posts">
       {posts.map((p) => (
-        <Post key ="{p.id}" post={p} />
+        <Post post={p} />
       ))}
     </div>
   );
--- client/src/context/Context.js
+++ client/src/context/Context.js
@@ -1,7 +1,6 @@
 import { createContext, useEffect, useReducer } from "react";
 import Reducer from "./Reducer";
 
-
 const INITIAL_STATE = {
   user: JSON.parse(localStorage.getItem("user")) || null,
   isFetching: false,
