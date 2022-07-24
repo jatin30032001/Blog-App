195b87ed56c5c0eb54c5dbfffb80730beb51d424
jatin30032001 <jrsaini2090@gmail.com>
2022-07-24 12:51:49 +0530

    Login and Update account

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,16 @@
-import Topbar from "./components/topbar/Topbar";
 import Homepage from "./pages/homepage/Homepage";
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
-import Settings from "./pages/settings/Settings";
+import Topbar from "./components/topbar/Topbar";
 import Single from "./pages/single/Single";
 import Write from "./pages/write/Write";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { useContext } from "react";
+import { Context } from "./context/Context";
 
 function App() {
-  const currentUser = false;
+  const { user } = useContext(Context);
   return (
     <Router>
       <Topbar />
@@ -16,19 +18,12 @@ function App() {
         <Route exact path="/">
           <Homepage />
         </Route>
-        <Route path="/posts">
-          <Homepage />
-        </Route>
-        <Route path="/register">
-          {currentUser ? <Homepage /> : <Register />}
-        </Route>
-        <Route path="/login">{currentUser ? <Homepage /> : <Login />}</Route>
-        <Route path="/post/:id">
+        <Route path="/register">{user ? <Homepage /> : <Register />}</Route>
+        <Route path="/login">{user ? <Homepage /> : <Login />}</Route>
+        <Route path="/write">{user ? <Write /> : <Register />}</Route>
+        <Route path="/settings">{user ? <Settings /> : <Register />}</Route>
+        <Route path="/post/:postId">
           <Single />
-        </Route>
-        <Route path="/write">{currentUser ? <Write /> : <Login />}</Route>
-        <Route path="/settings">
-          {currentUser ? <Settings /> : <Login />}
         </Route>
       </Switch>
     </Router>
